b6c60b54e2783baf14961856b00d24cd29b7296c
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-26 22:42:30 +0900

    feat: recreateGeneration 생성

--- src/main/java/geneticAlgorithm/Simulation.java
+++ src/main/java/geneticAlgorithm/Simulation.java
@@ -1,11 +1,13 @@
 package geneticAlgorithm;
 
+import utils.RandomUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Simulation {
 
-    public List<Gene> createGeneration(int population){
+    public List<Gene> createGeneration(int population) {
         List<Gene> generation = new ArrayList<>();
         for (int i = 0; i < population; i++) {
             generation.add(new Gene());
@@ -13,4 +15,19 @@ public class Simulation {
         return generation;
     }
 
+    public List<Gene> recreateGeneration(List<Gene> oldGeneration) {
+        List<Gene> newGeneration = null;
+        for (Gene gene : oldGeneration) {
+            newGeneration.add(gene.mate(getPartner(oldGeneration, gene)));
+        }
+        return newGeneration;
+    }
+
+    private Gene getPartner(List<Gene> society, Gene loner) {
+        Gene partner = society.get(RandomUtils.getRandomIndex(society.size()));
+        if (partner == loner) {
+            return getPartner(society, loner);
+        } else
+            return partner;
+    }
 }
