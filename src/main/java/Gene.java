6f7f33051dad30a364ba67a2c95e8dc36dec302f
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-26 21:34:39 +0900

    feat: chromosome getter 생성

--- src/main/java/Gene.java
+++ src/main/java/Gene.java
@@ -7,6 +7,10 @@ public class Gene {
         chromosome = generateGene();
     }
 
+    public String getChromosome() {
+        return chromosome;
+    }
+
     private String generateGene() {
         String result = "";
         for (int i = 0; i < 8; i++) {
