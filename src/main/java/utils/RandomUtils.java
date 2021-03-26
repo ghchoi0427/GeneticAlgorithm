8ffdf7407887f2256fe68f94b97fc90e5dc2813e
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-26 21:42:03 +0900

    fix: random 함수 수정

--- src/main/java/utils/RandomUtils.java
+++ src/main/java/utils/RandomUtils.java
@@ -6,6 +6,6 @@ public class RandomUtils {
     private final static Random random = new Random();
 
     public static String getRandomBinary(){
-        return String.valueOf(random.nextInt(1));
+        return String.valueOf(random.nextInt(2));
     }
 }
