b8777fdc79736eadb8854d81268d997e054ca253
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-26 23:10:49 +0900

    feat: inputSettings 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,14 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
 public class InputView {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public int[] inputSettings() throws Exception{
+        String[] line = bufferedReader.readLine().split(",");
+         return Stream.of(line).mapToInt(Integer::parseInt).toArray();
+    }
 }
