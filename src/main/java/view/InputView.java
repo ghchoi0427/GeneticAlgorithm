cc1ea776cd452dc2afec9b5983fbfcf6f2e72cf4
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-26 23:15:09 +0900

    refactor: inputNumber 로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,13 +2,11 @@ package view;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
 
 public class InputView {
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public int[] inputSettings() throws Exception{
-        String[] line = bufferedReader.readLine().split(",");
-         return Stream.of(line).mapToInt(Integer::parseInt).toArray();
+    public int inputNumber() throws Exception {
+        return Integer.parseInt(bufferedReader.readLine());
     }
 }
