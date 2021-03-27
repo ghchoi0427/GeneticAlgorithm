cbb6a477890f5e0fb623a77b24afd7017ddcae8b
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-27 11:29:21 +0900

    refactor: private static 으로 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class InputView {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    public int inputNumber() throws Exception {
+    public static int inputNumber() throws Exception {
         return Integer.parseInt(bufferedReader.readLine());
     }
 }
