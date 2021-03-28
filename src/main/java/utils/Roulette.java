998711fe500156c7f0d391b5fe7dca9052cdb3ec
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-28 16:03:27 +0900

    refactor: 필요없는 기능 제거

--- src/main/java/utils/Roulette.java
+++ src/main/java/utils/Roulette.java
@@ -2,22 +2,11 @@ package utils;
 
 import geneticAlgorithm.Gene;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Roulette {
 
     private Roulette() {
-    }
-
-    public static List<Integer> getOverallFitness(List<Gene> geneList) {
-        List<Integer> fitnesses = new ArrayList<>();
-        for (Gene gene : geneList) {
-            fitnesses.add(gene.getFitness());
-        }
-        return fitnesses.stream().distinct().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public static int getFitnessSum(List<Gene> geneList) {
