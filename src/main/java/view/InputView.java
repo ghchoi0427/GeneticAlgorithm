58c3256610a070f6fbd0a66663706f7e95531fb0
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-28 14:16:10 +0900

    feat: input 메서드 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner sc = new Scanner(System.in);
+    private static final String INPUT_POPULATION = "input population: ";
+    private static final String INPUT_GENERATION = "input generation: ";
+    private static final String INPUT_MUTATION = "input mutation(%): ";
 
     public static int inputNumber() {
+        return sc.nextInt();
+    }
+
+    public static int inputPopulation() {
+        System.out.println(INPUT_POPULATION);
+        return sc.nextInt();
+    }
+
+    public static int inputGeneration() {
+        System.out.println(INPUT_GENERATION);
+        return sc.nextInt();
+    }
+
+    public static int inputMutation() {
+        System.out.println(INPUT_MUTATION);
         return sc.nextInt();
     }
 }
