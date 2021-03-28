6d5ca3b62a76ad3728a4a3ebf48a51ad8624830b
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-28 15:40:27 +0900

    refactor: 메시지 명시

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 public class OutputView {
 
-    public static void averageFitness(List<Gene> geneList){
-        int IdealFitnessSum = geneList.get(0).getChromosome().length()* geneList.size();
+    private static final String MESSAGE_AVERAGE_FITNESS = "average fitness rate: ";
+
+    public static void averageFitness(List<Gene> geneList) {
+        int IdealFitnessSum = geneList.get(0).getChromosome().length() * geneList.size();
         int currentFitnessSum = Roulette.getFitnessSum(geneList);
 
-        System.out.println("average fitness rate: "+(float)(currentFitnessSum * 100 / IdealFitnessSum) + "%");
+        System.out.println(MESSAGE_AVERAGE_FITNESS + (float) (currentFitnessSum * 100 / IdealFitnessSum) + "%");
     }
 }
