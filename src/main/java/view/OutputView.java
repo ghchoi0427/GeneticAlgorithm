a9a4d63aad264ef1c358c2f1d64cbe5225674429
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-28 15:34:05 +0900

    feat: averageFitness 메서드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,16 @@
 package view;
 
+import geneticAlgorithm.Gene;
+import utils.Roulette;
+
+import java.util.List;
+
 public class OutputView {
+
+    public static void averageFitness(List<Gene> geneList){
+        int IdealFitnessSum = geneList.get(0).getChromosome().length()* geneList.size();
+        int currentFitnessSum = Roulette.getFitnessSum(geneList);
+
+        System.out.println("average fitness rate: "+(float)(currentFitnessSum * 100 / IdealFitnessSum) + "%");
+    }
 }
