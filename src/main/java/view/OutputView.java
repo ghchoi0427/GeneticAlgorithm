58d4dcdc0040e31a0873595078b8ed8571ddc116
ghchoi0427 <ghchoi0427@gmail.com>
2021-03-28 15:57:59 +0900

    feat: getTheFittest 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,10 +4,11 @@ import geneticAlgorithm.Gene;
 import utils.Roulette;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class OutputView {
 
-    private static final String MESSAGE_AVERAGE_FITNESS = "average fitness rate: ";
+    private static final String MESSAGE_AVERAGE_FITNESS = "average fitness: ";
 
     public static void averageFitness(List<Gene> geneList) {
         int IdealFitnessSum = geneList.get(0).getChromosome().length() * geneList.size();
@@ -16,7 +17,11 @@ public class OutputView {
         System.out.println(MESSAGE_AVERAGE_FITNESS + (float) (currentFitnessSum * 100 / IdealFitnessSum) + "%");
     }
 
-    public static void generationIndicator(int index){
+    public static void generationIndicator(int index) {
         System.out.println("[Generation " + index + "]");
+    }
+
+    public static void getTheFittest(List<Gene> geneList) {
+        System.out.println("The Fittest: " + geneList.stream().mapToInt(e -> e.getFitness()).max().orElseThrow(NoSuchElementException::new) + "/" + geneList.get(0).getChromosome().length());
     }
 }
